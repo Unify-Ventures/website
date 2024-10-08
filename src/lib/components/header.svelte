36552d27419784f93c57bd60632273ec8cc39bde
pyscripter99 <ryder@retzlaff.family>
2024-10-09 09:52:12 +1100

    Improve header sizes and spacing

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,12 +3,12 @@
     import { Backpack } from "lucide-svelte";
 </script>
 
-<div class="flex justify-between items-center p-6">
-    <Logo id="logo" class="w-36 h-min m-2" />
+<div class="flex justify-between items-center p-2">
+    <Logo id="logo" class="w-28 h-min m-2" />
     <div class="w-full"></div>
     <div class="flex items-center gap-4">
         <button>
-            <Backpack size="32" aria-label="Backpack" />
+            <Backpack size="28" class="mr-2" aria-label="Backpack" />
         </button>
     </div>
 </div>
