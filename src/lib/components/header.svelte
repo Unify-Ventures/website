1391c371c83411963533ef08d8e9455a6d5826de
pyscripter99 <ryder@retzlaff.family>
2024-10-20 18:27:22 +1100

    Remove toolbox

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -5,10 +5,4 @@
 
 <div class="flex justify-between items-center p-2">
     <Logo id="logo" class="w-28 h-min m-2" />
-    <div class="w-full"></div>
-    <div class="flex items-center gap-4">
-        <button>
-            <Backpack size="28" class="mr-2" aria-label="Backpack" />
-        </button>
-    </div>
 </div>
