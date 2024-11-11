7ac6bce73535f8a9f12747e4a97fe23cc3ef1796
pyscripter99 <ryder@retzlaff.family>
2024-11-11 16:34:08 +1100

    Add terms page

---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,94 @@
+<section class="section">
+    <div>
+        <h2>Terms</h2>
+        <div class="flex w-full flex-col lg:flex-row gap-12 lg:gap-0">
+            <div class="prose max-w-none prose-lg">
+                <div class="space-y-4">
+                    <p>
+                        This website is operated by and on behalf of Unify
+                        Ventures Pty Ltd (ABN 24 659 500 222) (<strong
+                            >Unify</strong
+                        >). Unify is a Corporate Authorised Representative (CAR
+                        No. 1299709) of Quay Wholesale Fund Services Pty Ltd
+                        (AFSL No. 528526) (<strong>QUAY</strong>). Your access
+                        to this website is subject to these terms and conditions
+                        (<strong>Terms</strong>) and by using this website you
+                        agree to the Terms. This website and the products and
+                        services described may be amended at any time without
+                        notice. Unify is authorised to provide financial
+                        services to wholesale clients only (as defined under
+                        s761G or s761GA of the Corporations Act 2001 (Cth)).
+                    </p>
+
+                    <p>
+                        This website is not designed to provide personal
+                        financial product advice to any person. No aspect of
+                        this website takes into account the objectives,
+                        financial situation or needs of any person. Before
+                        making an investment decision, you should read the
+                        relevant offer document and (if appropriate) seek
+                        professional advice to determine whether the investment
+                        is suitable for you.
+                    </p>
+
+                    <p>
+                        Any managed investment fund product (<strong
+                            >Fund</strong
+                        >) offered via this website is offered via an
+                        information memorandum which contains all the details of
+                        the offer. Before making any decision to make or hold
+                        any investment in the Fund you should consider the
+                        information memorandum in full. An investment may
+                        achieve a lower than expected return and investors risk
+                        losing some or all of their principal investment. Past
+                        performance is no indication of future performance.
+                    </p>
+
+                    <p>
+                        This website does not constitute an offer to sell or a
+                        solicitation of an offer to buy any product or service
+                        in any jurisdiction (i) in which such offer or
+                        solicitation is not authorised, or (ii) in which the
+                        person making the offer is not qualified to do so, or
+                        (iii) to whom it is unlawful to make such offer or
+                        solicitation.
+                    </p>
+
+                    <p>
+                        Unify makes no representation or warranty as to whether
+                        the content of this website is accurate, complete or up
+                        to date. To the maximum extent permitted by law, Unify
+                        and its related parties will not be liable in any way
+                        for any loss or damage suffered by you through use or
+                        access to this website, or Unify's failure to provide
+                        this website. Any projections contained in this website
+                        are estimates only and may not be realised in the
+                        future.
+                    </p>
+
+                    <p>
+                        Except where necessary for viewing the information on
+                        this website on your browser, no content from this
+                        website may be reproduced in any manner, and no further
+                        dissemination of it is permitted, without the express
+                        written consent of Unify.
+                    </p>
+
+                    <p>
+                        The copyright on all information contained on this site
+                        is owned by Unify.
+                    </p>
+
+                    <p>
+                        This website may contain links to other websites. Unify
+                        is not responsible for the content of linked websites.
+                        The inclusion of these links does not in any way imply
+                        any form of endorsement by Unify of the products or
+                        services provided by persons or organisations who are
+                        responsible for the linked websites.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
