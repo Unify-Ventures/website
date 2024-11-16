ec21c6c282445b19a5517bcba83e0092288f2670
pyscripter99 <ryder@retzlaff.family>
2024-11-16 14:17:20 +1100

    Add portfolio page

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -6,10 +6,16 @@
     } from "$lib/pb-types";
     import { getFileUrl, pb } from "$lib/pocketbase";
     import { inlineSvg } from "@svelte-put/inline-svg";
-    import type { FilterConfig } from "@zshzebra/svelte-multi-filter";
+    import {
+        createFilterStore,
+        type FilterConfig,
+        type FilterDimension,
+        type FilterStore,
+    } from "@zshzebra/svelte-multi-filter";
     import { onMount } from "svelte";
 
     const stages = [
+        { value: "Any", label: "Any" },
         ...Object.keys(PortfolioCompaniesStageOptions).map((stage) => {
             return {
                 value: stage,
@@ -27,53 +33,130 @@
     }
 
     let portfolios = $state<PortfolioCompaniesResponse[]>([]);
+    let filterablePortfolios = $state<PortfolioFilter[]>([]);
 
-    interface ProductFilter {
-        stage: 	'product_launch' | 'market_validation' | 'scaling' | 'revenue_momentum' | 'liquidity_event',
+    interface PortfolioFilter {
+        id: string;
+        stage:
+            | "product_launch"
+            | "market_validation"
+            | "scaling"
+            | "revenue_momentum"
+            | "liquidity_event";
+        fund:
+            | "0jz3m1u87fm8c5j"
+            | "l7bnwnwwzs3yr4n"
+            | "4f6b55zzbes47nj"
+            | "h8c2tlfs7gt29nv";
     }
 
     const config = {
-        stage: ['product_launch', 'market_validation', 'scaling', 'revenue_momentum', 'liquidity_event'],
-    } satisfies FilterConfig<ProductFilter>;
+        stage: [
+            "product_launch",
+            "market_validation",
+            "scaling",
+            "revenue_momentum",
+            "liquidity_event",
+        ],
+        fund: [
+            "0jz3m1u87fm8c5j",
+            "l7bnwnwwzs3yr4n",
+            "4f6b55zzbes47nj",
+            "h8c2tlfs7gt29nv",
+        ],
+    } satisfies Record<string, any>;
+
+    let portfolioMap = $state<Record<string, PortfolioCompaniesResponse>>({});
+
+    let filterStore: FilterStore<Record<string, any>>;
+
+    let dimensions = $state<{
+        stage: FilterDimension<string>;
+        fund: FilterDimension<string>;
+    }>();
+    let results = $state<PortfolioCompaniesResponse[]>([]);
+    let availableStageOptions = $state<String[]>([]);
+    let availableFundOptions = $state<String[]>([]);
 
     onMount(async () => {
         portfolios = await pb
             .collection(Collections.PortfolioCompanies)
             .getFullList();
 
+        filterablePortfolios = portfolios.map((p) => ({
+            id: p.id,
+            stage: p.stage,
+            fund: p.funds[0],
+        }));
+
+        portfolios.forEach((p) => {
+            portfolioMap[p.id] = p;
+        });
+
         let fundsResp = await pb.collection(Collections.Funds).getFullList();
 
         funds = [
-            { value: "any", label: "Any" },
+            { value: "Any", label: "Any" },
             ...fundsResp.map((fund) => ({
                 value: fund.id,
                 label: fund.name,
             })),
         ];
+
+        filterStore = createFilterStore(filterablePortfolios, config);
+
+        filterStore.subscribe((dims) => {
+            dimensions = dims;
+        });
+
+        filterStore.filteredItems.subscribe((items) => {
+            results = items;
+        });
+
+        filterStore.getAvailableOptions("stage").subscribe((options) => {
+            availableStageOptions = options;
+        });
+
+        filterStore.getAvailableOptions("fund").subscribe((options) => {
+            availableFundOptions = options;
+        });
     });
 </script>
 
 <div class="grid place-content-center">
     <h2 class="text-7xl font-medium">Portfolio</h2>
-    <div class="relative w-full max-w-7xl lg:m-8 m-4">
-        <div class="w-0 h-0 sticky -left-0 top-10">
+    <div class="relative w-full max-w-7xl lg:m-8 m-4 flex gap-4">
+        <!-- Selection filters -->
+        <div class="w-0 h-0 sticky left-0 top-10">
             <div class="border-2 p-4 border-zinc-700 bg-white w-64">
                 <div class="flex flex-col">
                     <h3 class="font-bold text-xl">Stage</h3>
                     {#each stages as stage}
-                        <div class="flex flex-row gap-2 items-center p-1" s>
+                        <div class="flex flex-row gap-2 items-center p-1">
                             <input
                                 type="radio"
                                 name="stage"
                                 value={stage.value}
                                 class="appearance-none w-3 h-3 rounded-full border border-zinc-900 focus:bg-zinc-900 checked:bg-zinc-900 disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed"
                                 id={`stage-${stage.value}`}
+                                disabled={!availableStageOptions.includes(
+                                    stage.value
+                                ) && stage.value !== "Any"}
+                                onchange={() => {
+                                    filterStore.select("stage", stage.value);
+                                }}
+                                checked={dimensions?.stage.selected ===
+                                    stage.value}
                             />
-                            <label for={`stage-${stage.value}`} class="cursor-pointer"
+                            <label
+                                for={`stage-${stage.value}`}
+                                class="cursor-pointer"
                                 >{stage.label}
                                 {portfolios
                                     .map((p) => p.stage)
-                                    .includes(stage.value as PortfolioCompaniesStageOptions)
+                                    .includes(
+                                        stage.value as PortfolioCompaniesStageOptions
+                                    )
                                     ? `(${portfolios.filter((p) => p.stage === stage.value).length})`
                                     : ""}</label
                             >
@@ -88,21 +171,58 @@
                                 value={fund.value}
                                 class="appearance-none w-3 h-3 rounded-full border border-zinc-900 focus:bg-zinc-900 checked:bg-zinc-900 disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed"
                                 id={`fund-${fund.value}`}
+                                disabled={!availableFundOptions.includes(
+                                    fund.value
+                                ) && fund.value !== "Any"}
+                                onchange={() => {
+                                    filterStore.select("fund", fund.value);
+                                }}
+                                checked={dimensions?.fund.selected ===
+                                    fund.value}
                             />
                             <label for={`fund-${fund.value}`}
                                 >{fund.label}
                                 {portfolios
                                     .map((p) => p.funds)
                                     .filter((f) => f.includes(fund.value))
-                                    .length ? `(${portfolios.map((p) => p.funds).filter((f) => f.includes(fund.value)).length})` : ""}
-                                </label>
+                                    .length
+                                    ? `(${portfolios.map((p) => p.funds).filter((f) => f.includes(fund.value)).length})`
+                                    : ""}
+                            </label>
                         </div>
                     {/each}
                 </div>
             </div>
         </div>
 
-        <main class="lg:ml-64">
+        <!-- Portfolio results -->
+        <main
+            class="lg:ml-64 grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 gap-4"
+        >
+            {#each portfolios as result}
+                {#if portfolios.find((p) => p.id === result.id)}
+                    <a
+                        href={portfolioMap[result.id].homepage}
+                        target="_blank"
+                        class="border-2 border-zinc-900 bg-white p-4 group"
+                        class:hidden={!results.some((r) => r.id === result.id)}
+                        aria-label="View {result.name} portfolio"
+                    >
+                        <div class="">
+                            <svg
+                                class="portfolio w-full h-full"
+                                style:--accent={portfolioMap[result.id].accent}
+                                width="16rem"
+                                height="16rem"
+                                use:inlineSvg={getFileUrl(
+                                    portfolioMap[result.id],
+                                    portfolioMap[result.id].logo
+                                )}
+                            />
+                        </div>
+                    </a>
+                {/if}
+            {/each}
         </main>
     </div>
 </div>
@@ -112,7 +232,7 @@
         color: var(--accent);
     }
 
-    input:disabled+label {
+    input:disabled + label {
         opacity: 0.5;
         cursor: not-allowed;
     }
