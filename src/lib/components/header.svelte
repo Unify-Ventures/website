0d7fd75f6d5f1441f160d499c6a004100ba2224d
Ryder <ryder@retzlaff.family>
2025-10-26 14:11:42 +1100

    feat: Show/hide navbar based on scroll direction

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
     import Logo from "$lib/components/logo.svelte";
+    import { onMount } from "svelte";
+
+    let scrollPosition = 50;
+    let isScrollingUp = $state(true);
+
+    onMount(() => {
+        window.addEventListener("scroll", () => {
+            isScrollingUp =
+                document.body.getBoundingClientRect().top > scrollPosition;
+            scrollPosition = document.body.getBoundingClientRect().top;
+        });
+    });
+
+    $effect(() => {
+        $inspect(isScrollingUp);
+    });
 </script>
 
-<div class="flex justify-between items-center p-2">
+<nav
+    class={"sticky top-0 justify-between items-center bg-white z-20 p-2 transition-all duration-200 w-full"}
+    class:-translate-y-full={!isScrollingUp}
+>
     <a href="/" aria-label="Home">
         <Logo id="logo" class="w-28 h-min m-2" />
     </a>
-</div>
+</nav>
