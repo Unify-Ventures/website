6390af44c1abdf7cf406f779d8364b43dd44c04a
Ryder <ryder@retzlaff.family>
2025-11-01 16:21:05 +1100

    fix: Revert header to be fixed to the page instead of the viewport

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,26 +1,10 @@
 <script lang="ts">
     import Logo from "$lib/components/logo.svelte";
     import { onMount } from "svelte";
-
-    let scrollPosition = 50;
-    let isScrollingUp = $state(true);
-
-    onMount(() => {
-        window.addEventListener("scroll", () => {
-            isScrollingUp =
-                document.body.getBoundingClientRect().top > scrollPosition;
-            scrollPosition = document.body.getBoundingClientRect().top;
-        });
-    });
-
-    $effect(() => {
-        $inspect(isScrollingUp);
-    });
 </script>
 
 <nav
-    class={"sticky top-0 justify-between items-center bg-white z-20 p-2 transition-all duration-200 w-full"}
-    class:-translate-y-full={!isScrollingUp}
+    class={"justify-between items-center bg-white z-20 p-2 transition-all duration-200 w-full"}
 >
     <a href="/" aria-label="Home">
         <Logo id="logo" class="w-28 h-min m-2" />
